bd192b3053e4535bc745037373d08d0b5eb61754
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 23:54:30 +0530

    5. Slicing Strings.py

---
+++ List/3.Slicing/5. Slicing Strings.py
@@ -0,0 +1,8 @@
+# Slicing works similarly with strings:
+
+my_string = "Hello, World!"
+print("My string contain has : ",my_string)
+
+sliced_string = my_string[7:12]  
+print("After slicing string to get characters from index 7 to 11 : ",sliced_string) 
+print()
