52252ee4e783a326039bea2471f404e646bdd578
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 23:01:42 +0530

    7. index( ).py

---
+++ List/2.Methods/7. index( ).py
@@ -0,0 +1,8 @@
+# index(): Finds the index of the first occurrence of an item/element
+
+my_list = [1, 2, 3]
+print(my_list)
+index = my_list.index(2)
+print("Index of element '2' in my_list:")
+print(index)
+print()
