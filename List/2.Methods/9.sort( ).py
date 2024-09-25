8ecc4345d877a2a0390dcdce375e4d7256fe4f49
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 23:04:28 +0530

    9.sort( ).py

---
+++ List/2.Methods/9.sort( ).py
@@ -0,0 +1,8 @@
+# sort(): Sorts the list in ascending order
+
+num = [8, 2, 1, 4, 5]
+print("unsorted list named num contains element : ",num)
+
+num_sorted = sorted(num)
+print("\nSorted list:", num_sorted)
+print()
