6a300beaeb9dbfc69fb91e10a981de3930db8a4a
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 22:56:58 +0530

    5.pop( ).py

---
+++ List/2.Methods/5.pop( ).py
@@ -0,0 +1,8 @@
+# pop(): Removes and returns the item at the specified index , (or the last item if no index is specified).
+
+listing = [12,35,46,58,25,64]
+print(listing)
+
+listing.pop(2)
+print("\nList after popping the element at index 2:")
+print(listing)
