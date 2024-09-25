52fc46e6b583e15fb81a4d78ce9a59d2497a91a3
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 23:03:06 +0530

    8.count( ).py

---
+++ List/2.Methods/8.count( ).py
@@ -0,0 +1,8 @@
+# count(): Counts the occurrences of a specified item/element
+
+my_list = [1, 2, 2, 3]
+print(my_list)
+
+count = my_list.count(2)
+print("\nCount of '2' in my_list:", count)
+print()
