437db307fb657e56d92b4b27235353cd7d135232
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 22:51:06 +0530

    4.remove( ).py

---
+++ List/2.Methods/4.remove( ).py
@@ -0,0 +1,8 @@
+# remove(): Removes the first occurrence of a specified item
+
+listing = [12,35,46,58,35,64]
+print(listing)
+listing.remove(35)
+print("\nList after removing 35:")
+print(listing)
+
