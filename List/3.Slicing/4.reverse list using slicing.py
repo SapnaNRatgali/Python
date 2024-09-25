b9a90f2f7db91a24cc717b09969c87acb0210e7f
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-25 23:51:39 +0530

    4.reverse list using slicing.py

---
+++ List/3.Slicing/4.reverse list using slicing.py
@@ -0,0 +1,9 @@
+# Reversing a List using slicing with a negative step:
+# my_list[::-1] starts from the end and moves backward through the list.
+
+my_list = [10, 20, 30, 40, 50]
+print("My list conatins : ",my_list)
+
+reversed_list = my_list[::-1]  
+print("After reversing my list : ",reversed_list)  
+print()
