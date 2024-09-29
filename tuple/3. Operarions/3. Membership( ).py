48a36e8617fbd1b9161529c9919f9ba1eb03dc82
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:54:24 +0530

    3. Membership( ).py

---
+++ tuple/3. Operarions/3. Membership( ).py
@@ -0,0 +1,11 @@
+# Membership (in, not in) -  Checks for the existence of a value.
+
+tup1 = (10, 20, 30, 40, 50, 10, 10)  
+tup2 = (11, 22, 33, 44, 55)
+
+# Check if an element exists in the tuple using the 'in' keyword.
+print("Is 22 in tup1?", 22 in tup1)  # Output: False
+print("Is 22 in tup2?", 22 in tup2)  # Output: True
+print()
+
+
