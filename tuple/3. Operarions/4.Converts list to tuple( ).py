5e66518045dbd33b3ec9dfc10ad4b8b7d3f145a8
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-30 00:00:41 +0530

    4.Converts list to tuple( ).py

---
+++ tuple/3. Operarions/4.Converts list to tuple( ).py
@@ -0,0 +1,8 @@
+# tuple(): Convert an iterable (like a list) into a tuple.
+
+nameList = ["apple", "mango", "grapes"]
+print("The list elements are:", nameList)
+
+nameTuple = tuple(nameList)  # Convert list to tuple
+print("The tuple elements are:", nameTuple)
+print()
