335b3b3a46aa01415b054dff57c277852c85d7c1
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:49:09 +0530

    1. Concatenation( ).py

---
+++ tuple/3. Operarions/1. Concatenation( ).py
@@ -0,0 +1,8 @@
+# You can combine two or more tuples using the (+) operator.
+
+tup1 = (10, 20, 30, 40, 50, 10, 10)  
+tup2 = (11, 22, 33, 44, 55)
+
+combine = tup1 + tup2
+print("Concatenated Tuple:", combine)
+print()
