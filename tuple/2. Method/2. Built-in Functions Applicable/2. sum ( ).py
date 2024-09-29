4ab9a754c1374bca72cc44c1dbafcc86b23209f2
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:30:14 +0530

    2. sum ( ).py

---
+++ tuple/2. Method/2. Built-in Functions Applicable/2. sum ( ).py
@@ -0,0 +1,10 @@
+# sum(tuple) - Returns the sum of all numeric elements.
+
+my_tuple = (1, 2, 3)  # Tuple containing numeric values
+
+# Using sum() method
+
+total = sum(my_tuple)  # Result: total = 6
+print("The sum of all elements in my_tuple is:", total)
+
+print()
