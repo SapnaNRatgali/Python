9b01c3585dab6cafb66ac5587c9180f82aa39c93
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-30 00:01:40 +0530

    4.Converts list to tuple( ).py

--- tuple/3. Operarions/4.Converts list to tuple( ).py
+++ tuple/3. Operarions/4.Converts list to tuple( ).py
@@ -6,3 +6,9 @@ print("The list elements are:", nameList)
 nameTuple = tuple(nameList)  # Convert list to tuple
 print("The tuple elements are:", nameTuple)
 print()
+
+
+# Another example of converting a list to a tuple
+myList = [1, 2, 3]
+myTuple = tuple(myList)  # Convert list to tuple
+print("Converted tuple from list:", myTuple)
