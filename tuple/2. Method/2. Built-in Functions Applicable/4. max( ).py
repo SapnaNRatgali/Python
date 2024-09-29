894c8a57caa69adadc53796738497662873b2bab
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:36:07 +0530

    4. max( ).py

---
+++ tuple/2. Method/2. Built-in Functions Applicable/4. max( ).py
@@ -0,0 +1,6 @@
+# max(tuple) - Returns the largest element.
+
+my_tuple = (10, 12, 30, 52, 32)
+maximum = max(my_tuple)  # Maximum value in my_tuple
+print("The maximum value in my_tuple is:", maximum)
+print()
