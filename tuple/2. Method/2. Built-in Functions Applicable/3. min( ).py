84e15a772aef5feb7e8995c3051280ccb080c146
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:33:33 +0530

    3. min( ).py

---
+++ tuple/2. Method/2. Built-in Functions Applicable/3. min( ).py
@@ -0,0 +1,7 @@
+# min(tuple) - Returns the smallest element.
+
+my_tuple = (15, 12, 10, 26, 21)
+
+minimum = min(my_tuple)  # Minimum value in my_tuple
+print("The minimum value in my_tuple is:", minimum)
+print()
