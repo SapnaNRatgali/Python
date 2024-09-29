ae0d50b4c9811ab6606f2aaecaa06769c64a80be
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-29 23:51:54 +0530

    2 repetition( ).py

---
+++ tuple/3. Operarions/2 repetition( ).py
@@ -0,0 +1,7 @@
+# Repeats the tuple a specified number of times.
+
+# You can repeat a tuple using the * operator.
+tup1 = (10, 20, 30, 40, 50, 10, 10) 
+repetition = tup1 * 2
+print("Repetition of tup1:", repetition)
+print()
