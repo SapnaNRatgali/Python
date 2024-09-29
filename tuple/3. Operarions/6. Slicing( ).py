0c31d7470ab1ab3cd3e3d3ad2ae4e439f129abb7
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-30 00:07:17 +0530

    6. Slicing( ).py

---
+++ tuple/3. Operarions/6. Slicing( ).py
@@ -0,0 +1,12 @@
+# Slicing ([:]) - Extracts a portion of the tuple.
+
+# Syntax: tuple[start:stop:step]
+
+# start: The index to start the slice (inclusive).
+# stop: The index to end the slice (exclusive).
+# step: The interval between elements (optional).
+
+t1 = (1, 2, 3, 4, 5)
+print(t1[1:4])  # Output: (2, 3, 4)
+print()
+
