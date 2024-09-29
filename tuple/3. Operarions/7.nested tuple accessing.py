704f3af4de41e090bced5dce355ff4f951e42f2d
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-09-30 00:15:12 +0530

    7.nested tuple accessing.py

---
+++ tuple/3. Operarions/7.nested tuple accessing.py
@@ -0,0 +1,6 @@
+# Accessing Elements in Nested Tuples
+
+# Tuples can contain other tuples, allowing nested structures.
+tup3 = ((90, 91, 92), (80, 81))
+print("Accessing element at index [0][1]:", tup3[0][1])  # Accessing the second element of the first nested tuple
+print()
