171cbfae3425c91588d83bb6b544636f01528c58
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-10-06 23:49:15 +0530

    5.Modulus.py

---
+++ 03.Operators/1. Arithmetic Operators/5.Modulus.py
@@ -0,0 +1,13 @@
+# Modulus : " % " 
+# It  returns the remainder of the division of two numbers. 
+
+print(10 % 3)  
+print() 
+
+print(25 % 4)   
+print()
+
+a = 20
+b = 25
+print(a % b)  # Modulus (remainder of division): 20
+print()
