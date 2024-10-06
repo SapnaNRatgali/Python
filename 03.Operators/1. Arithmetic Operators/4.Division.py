7151f7c7f91cc2a5845f12d171d3e8565ba0e99e
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-10-06 23:46:34 +0530

    4.Division.py

---
+++ 03.Operators/1. Arithmetic Operators/4.Division.py
@@ -0,0 +1,14 @@
+# Division: " / " - 
+# it will always returns a float value, even if both operands are integers. 
+
+print(10 / 2)  
+print()
+
+print(7 / 3)    
+print()
+
+a = 20
+b = 25
+
+print("division of a and b is : ",b / a)  
+print()
