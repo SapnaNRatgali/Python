a93fd93208fc5f94c987b913107ece4831982991
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-10-06 23:51:40 +0530

    6. Exponentiation.py

---
+++ 03.Operators/1. Arithmetic Operators/6. Exponentiation.py
@@ -0,0 +1,12 @@
+# Exponentiation : " ** "
+
+print(" exponentiation (2 raised to the power of 3) is : ",2 ** 3)  
+print()
+
+print("exponentiation (5 raised to the power of 2) is : ",5 ** 2)   
+print()
+
+a = 20
+b = 25
+print(a ** b)  # Exponentiation (a raised to the power of b): 20^25
+print()
