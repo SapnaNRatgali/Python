d18e2f5c90fc54e646093e351946fc26703ab193
SapnaNRatgali <ratgalisapna23@gmail.com>
2024-10-06 23:54:09 +0530

    7.Floor Division.py

---
+++ 03.Operators/1. Arithmetic Operators/7.Floor Division.py
@@ -0,0 +1,13 @@
+# Floor Division : " // "
+# it divides two numbers and returns the largest integer that is less than or equal to the result, effectively rounding down to the nearest whole number.
+
+print(10 // 3)   # (quotient of 10/3, rounded down)
+print()
+
+print(20 // 6)   
+print()
+
+a = 20
+b = 25
+print(b // a) # Floor Division (integer result): 1
+print()
